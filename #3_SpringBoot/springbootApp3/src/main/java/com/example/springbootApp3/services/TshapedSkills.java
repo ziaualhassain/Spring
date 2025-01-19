a9974e2abb3a7f0398cbfc3fcc4102ee10e6d58a
Zia <ziaualhassain@gmail.com>
2025-01-19 18:52:16 +0530

    Qualifer vs Primary Notes

--- #3_SpringBoot/springbootApp3/src/main/java/com/example/springbootApp3/services/TshapedSkills.java
+++ #3_SpringBoot/springbootApp3/src/main/java/com/example/springbootApp3/services/TshapedSkills.java
@@ -1,6 +1,7 @@
 package com.example.springbootApp3.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,14 +9,13 @@ public class TshapedSkills {
 	
 	private Icourse course;
 	
-	@Autowired
 	public void setCourse(Icourse course) {
 		System.out.println("setter injection");
 		this.course = course;
 	}
 	
-	
-	public TshapedSkills(Icourse course) {
+	@Autowired
+	public TshapedSkills(@Qualifier("springBoot") Icourse course) {
 		System.out.println("TshapedSkills Bean created");
 		System.out.println("Constructor injection");
 		this.course = course;
